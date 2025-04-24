a8bf30f97714717295f3ba589e0ece555c561ca3
NullPointerX <157622851+whitehai11@users.noreply.github.com>
2025-04-24 17:08:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.getElementById("toggleTheme").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({ duration: 800, once: true });
+
+  document.getElementById("toggleTheme").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
+
+  document.getElementById("downloadPDF").addEventListener("click", () => {
+    window.print();
+  });
 });
